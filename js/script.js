63095b3ae0d4ba7f4867182482643d9ec3f00410
silvialep <silvia.lepore_sl@libero.it>
2023-03-02 14:52:06 +0100

    feat: pali function created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// Palindroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
@@ -10,3 +8,37 @@
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+
+// Palindroma
+
+// Chiedere all’utente di inserire una parola
+let userWord = prompt('Inserisci una parola');
+
+// Creare una funzione per capire se la parola inserita è palindroma
+function paliWord() {
+    // debugger;
+    // console.log(userWord);
+
+    // creo variabile per dividere gli elementi della stringa
+    let wordSplit = userWord.split('');
+    // console.log(wordSplit);
+
+    // creo variabile per invertire gli elementi della stringa
+    let word = wordSplit.reverse();
+    // console.log(word);
+
+    // creo variabile per riunire gli elementi della nuova stringa invertita
+    let inverseWord = word.join('');
+    // console.log(inverseWord);
+
+    // verifico che le due parole siano uguali e stampo a console il risultato
+    if(userWord == inverseWord) {
+        console.log('La parola è palindroma');
+    } else {
+        console.log('La parola non è palindroma');
+    }
+
+}
+
+paliWord();
